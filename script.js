9bf2badf1de7c3c0d8cd0be24c6abeed7fe11e84
Anya Harris <aunnie@MacBookAir.home.local>
2025-03-05 20:29:12 -0500

    commit changes

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong', 'whoosh', 'heartbeat', 'crash'];
 
 sounds.forEach((sound) => {
     const btn = document.createElement('button');
     btn.classList.add('btn');
     btn.innerText = sound;
     document.getElementById('buttons').appendChild(btn);
-});
 
-btn.addEventListener('click', () => {
-    document.getElementById(sound).play();
+    btn.addEventListener('click', () => {
+        document.getElementById(sound).play();
+    });
 });
 
 function stopSounds() {
     sounds.forEach((sound) => {
         const song = document.getElementById(sound);
-
         song.pause();
         song.currentTime = 0;
     });
